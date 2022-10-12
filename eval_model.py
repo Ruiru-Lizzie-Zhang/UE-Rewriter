d3e4a4bd6ef8bf07ff4e0f8c3332069aa76f36b2
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2022-10-12 01:15:51 -0400

    eval on batches

---
+++ eval_model.py
@@ -0,0 +1,61 @@
+from preprocess import read_txt
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM  # AutoModelForCausalLM
+from tqdm import tqdm
+from bleu import corpus_bleu
+
+import torch
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+import argparse
+from argparse import RawTextHelpFormatter
+def parse_option():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--data_dir', type=str, default='../wizard_of_wikipedia/data.json')
+    parser.add_argument('--model_name', type=str, default="facebook/blenderbot_small-90M")
+    parser.add_argument('--num_beams', type=int, default=5)
+    parser.add_argument('--num_return_sequences', type=int, default=1)
+    parser.add_argument('--min_len_generated', type=int, default=10)
+    parser.add_argument('--max_len_generated', type=int, default=50)
+    parser.add_argument('--eval_batch_size', type=int, default=512)
+    parser.add_argument('--debug', type=bool, default=False)
+
+    opt = parser.parse_args()
+    return opt
+
+def main():
+    opt = parse_option()
+    
+    if opt.debug:
+        all_data = read_txt(opt.data_dir)[:2000] # needs to be txt input
+    else:
+        all_data = read_txt(opt.data_dir)
+
+    tokenizer = AutoTokenizer.from_pretrained(opt.model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(opt.model_name)
+
+    all_outputs = []
+    bleus = []
+    for batch in tqdm([all_data[i : i + opt.eval_batch_size] for i in range(0, len(all_data), opt.eval_batch_size)]):
+
+        input_ids = tokenizer(batch[:-1], padding='max_length', truncation=True, return_tensors="pt")["input_ids"]
+        input_ids = input_ids.to(DEVICE)
+        output_ids = model.generate(input_ids=input_ids, num_beams=5, num_return_sequences=1, min_length=10, max_length=50)
+        all_outputs.append(output_ids)
+
+        hyp = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+        ref = batch[1:]
+        bleu = corpus_bleu(hyp, ref)[0]
+        if sum([0 != i for i in bleu]) == 1:
+            if bleu[1] > 0:
+                bleus.append(bleu[1])
+            else:
+                bleus.append(bleu)
+        else:
+            bleus.append(bleu)
+        
+        torch.save(all_outputs, opt.model_name+'/output_ids.pt')
+        torch.save(bleus, opt.model_name+'/bleus.pt')
+
+    
+if __name__ == '__main__':
+    main()
--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
-def read_data(directory):
+def read_json(directory):
     
+    import json
     # read data as list of list, each smaller list is a dialog composing of X sentences (8<=X<=23 for Wizard of Wikipedia)
     
     docs = []
@@ -12,5 +13,18 @@ def read_data(directory):
     all_data = []
     for doc in docs:
         dialog = [i['text'].lower() for i in doc['dialog']]
-        all_data.append(dialog_lower)
+        all_data.append(dialog)
+    return all_data
+
+
+def read_txt(directory):
+    
+    # read data as list of sentences, separated by ##
+    
+    file = open(directory, 'r', encoding='utf-8')
+    file_data = file.read() 
+    all_data = file_data.split('\n')
+
+    while "" in all_data:
+        all_data.remove("")
     return all_data
