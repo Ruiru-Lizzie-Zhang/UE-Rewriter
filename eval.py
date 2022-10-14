721734a666559fa25003089c2e9d5b3ef8aafc63
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2022-10-14 19:29:26 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -10,6 +10,7 @@ def parse_option():
     parser.add_argument('--hyp_dir', type=str, default='blenderbot_small-90M_generate_original.txt')
     parser.add_argument('--ref_dir', type=str, default='all_data.txt')
     parser.add_argument('--eod_token', type=str, default='##')
+    parser.add_argument('--ignore_eod', type=bool, default=True)
     parser.add_argument('--debug', type=bool, default=False)
 
     opt = parser.parse_args()
@@ -38,18 +39,27 @@ def main():
         else:
             data.append(f.read().split('\n'))
         f.close()
+        
     hyp_data, ref_data = data
     if len(hyp_data) != len(ref_data):
         raise Exception(f"Hypotheses length {len(hyp_data)} does not match references length {len(ref_data)}.")
-    mask_ref = [i for i, j in enumerate(ref_data) if not (j == opt.eod_token or ref_data[i-1] == opt.eod_token or i == 0)]
-    mask_hyp = [i for i, j in enumerate(ref_data) if not (j == opt.eod_token or i == len(ref_data)-1 or ref_data[i+1] == opt.eod_token)]
-    if len(mask_hyp) != len(mask_ref):
-        raise Exception(f"Check whether each dialog is ended by {opt.eod_token}.")
-    hyp_data = list(itemgetter(*mask_hyp)(hyp_data))
-    ref_data = list(itemgetter(*mask_ref)(ref_data))
+    
+    if opt.ignore_eod:
+        mask_ref = [i for i, j in enumerate(ref_data) if not (j == opt.eod_token or ref_data[i-1] == opt.eod_token or i == 0)]
+        mask_hyp = [i for i, j in enumerate(ref_data) if not (j == opt.eod_token or i == len(ref_data)-1 or ref_data[i+1] == opt.eod_token)]
+        if len(mask_hyp) != len(mask_ref):
+            raise Exception(f"Check whether each dialog is ended by {opt.eod_token}.")
+        hyp_data = list(itemgetter(*mask_hyp)(hyp_data))
+        ref_data = list(itemgetter(*mask_ref)(ref_data))
+    
     ref_data = list(map(list, zip(ref_data)))
 
     bleu, addition = corpus_bleu(hyp_data, ref_data)
     print("BLEU = %.2f, BLEU_1 = %.1f, BLEU_2 =%.1f, BLEU_3 = %.1f, BLEU_4 = %.1f \
     (BP=%.3f, ratio=%.3f, hyp_len=%d, ref_len=%d)"%(bleu[0]*100, bleu[1]*100, bleu[2]*100, bleu[3]*100, bleu[4]*100, 
                                                     addition[0], addition[1], addition[2], addition[3]))
+    
+    
+    
+if __name__ == '__main__':
+    main()
