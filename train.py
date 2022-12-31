19a8762802ba31cedec3dadc9d2e18b90e94c989
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2022-12-31 15:49:37 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 # !pip install transformers
-# !pip install datasets
 # !pip install evaluate
 # !pip install rouge_score
 
@@ -32,7 +31,10 @@ def parse_option():
     opt = parser.parse_args()
     return opt
 
+opt = parse_option()
+print(opt)
 
+tokenizer = AutoTokenizer.from_pretrained("facebook/"+opt.model_name)
 def preprocess_function(examples):
     inputs = examples['input']
     outputs = examples['output']
@@ -73,22 +75,18 @@ def compute_metrics(eval_preds):
 
 
 def main():
-    opt = parse_option()
-    print(opt)
-    
+
     if not file_exist(opt.data_dir_json):
         from preprocess import txt_to_json_pair
         txt_to_json_pair(opt.data_dir_txt, opt.data_dir_json, opt.eod_token)
     
     dataset = load_dataset("json", data_files=opt.data_dir_json, split='train')
     dataset = dataset.train_test_split(test_size=0.2, seed=12345)
+    tokenized_datasets = dataset.map(preprocess_function).remove_columns(['input','output'])
 
     metric = evaluate.load(opt.metric)
-
-    tokenizer = AutoTokenizer.from_pretrained("facebook/"+model_name)
-    tokenized_datasets = dataset.map(preprocess_function).remove_columns(['input','output'])
     
-    model = AutoModelForSeq2SeqLM.from_pretrained("facebook/"+model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained("facebook/"+opt.model_name)
     model = torch.nn.DataParallel(model).to(DEVICE)
     
     training_args = Seq2SeqTrainingArguments(output_dir="trainer", 
